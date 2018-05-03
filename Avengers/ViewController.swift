69cd4f3bbcf94e1761605536b1a3497620188483
Khoa Pham <onmyway133@gmail.com>
2018-05-03 16:09:38 +0200

    Add controls

--- Avengers/ViewController.swift
+++ Avengers/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+  private lazy var selectButton: UIButton = {
+    let button = UIButton()
+    button.setTitle("Select image", for: .normal)
+    button.setTitleColor(.black, for: .normal)
+    return button
+  }()
 
+  private lazy var imageView: UIImageView = {
+    let view = UIImageView()
+    view.contentMode = .scaleAspectFit
+    return view
+  }()
+
+  private lazy var resultLabel: UILabel = {
+    let label = UILabel()
+    label.textAlignment = .center
+    label.textColor = .black
+
+    return label
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    setupControls()
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  private func setupControls() {
+    view.addSubview(selectButton)
+    view.addSubview(imageView)
+    view.addSubview(resultLabel)
+
+    NSLayoutConstraint.on(constraints: [
+      imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+      imageView.heightAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 1),
+      imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+
+      resultLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      resultLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+
+      selectButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      selectButton.topAnchor.constraint(equalTo: resultLabel.bottomAnchor, constant: 20)
+    ])
   }
 }
 
