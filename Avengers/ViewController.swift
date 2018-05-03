fef8334dcdb76d52acacb0007c5fe18ac3017c6c
Khoa Pham <onmyway133@gmail.com>
2018-05-03 15:58:52 +0200

    Initial commit

---
+++ Avengers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Avengers
+//
+//  Created by Khoa Pham on 03.05.2018.
+//  Copyright © 2018 Fantageek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# Avengers
+
+## Description
+
+- Demo how to use IBM Watson and CoreML
+- Read my blog post 
+
+## Author
+
+Khoa Pham, onmyway133@gmail.com
+
+## License
+
+**Avengers** is available under the MIT license. See the [LICENSE](https://github.com/onmyway133/Avengers/blob/master/LICENSE.md) file for more info.
