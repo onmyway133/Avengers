443f0b1aaaef23c460fcd56dda78fab24a6cf5db
Khoa Pham <onmyway133@gmail.com>
2018-05-03 16:02:38 +0200

    Add Constraints

---
+++ Avengers/Constraints.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension NSLayoutConstraint {
+  public static func on(constraints: [NSLayoutConstraint]) {
+    constraints.forEach {
+      ($0.firstItem as? UIView)?.translatesAutoresizingMaskIntoConstraints = false
+      $0.isActive = true
+    }
+  }
+}
+
